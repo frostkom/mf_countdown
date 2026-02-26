c2661d4672ae7e4cd93a3fd233855fee9ddb4ab7
Martin Fors <info@martinfors.se>
2026-02-26 19:53:15 +0100

    Make sure that numbers do not jump on every count

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Countdown
 Plugin URI: https://github.com/frostkom/mf_countdown
 Description: Add block with a countdown timer
-Version: 1.1.13
+Version: 1.1.14
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
